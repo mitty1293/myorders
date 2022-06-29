c78667160d1b899962bf5ed7baa83a29df6dcd1f
mitty1293 <k.fmichi@gmail.com>
2022-06-29 11:23:25 +0900

    修正

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@
 poetry run python manage.py makemigrations
 poetry run python manage.py migrate
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | poetry run python manage.py shell
+# poetry run python manage.py graph_models order_history -g -o erd.png
