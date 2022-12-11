305b8e761f8903074a29fbde362a6d11710b8eb6
mitty1293 <k.fmichi@gmail.com>
2022-12-11 16:30:11 +0900

    python-decoupleのインストールと設定

--- myorders/settings.py
+++ myorders/settings.py
@@ -12,14 +12,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
-from .env import *
+from decouple import config
+
+DEBUG = config("DJANGO_DEBUG", default=False, cast=bool)
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 if DEBUG:
     DATABASES = {
